4f3fee2cf807316d3693d2858a4c210fbabb37ed
Slinkter <luis.j.cueva@gmail.com>
2024-09-22 17:29:11 -0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
     Card,
     CardBody,
@@ -6,21 +6,33 @@ import {
     Typography,
     Button,
 } from "@material-tailwind/react";
-
-const url = "https://api.github.com/users";
-
+import { Spinner } from "@material-tailwind/react";
 const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
     const [user, setUser] = useState([]);
 
     const getUsers = async () => {
         try {
+            setIsLoading(true);
+            const url = "https://api.github.com/users";
             const res = await fetch(url);
             const data = await res.json();
+            if (!res.ok) {
+                throw Error("error en el res");
+            }
+            if (!res) {
+                throw Error("error en el res");
+            }
             setUser(data);
+            setIsError(null);
+            console.log(res);
+            console.log(data);
         } catch (error) {
+            setIsError(error);
             console.log(error);
         } finally {
-            console.log("finally");
+            setIsLoading(false);
         }
     };
 
@@ -28,23 +40,47 @@ const App = () => {
         getUsers();
     }, []);
 
+    if (isLoading) {
+        return (
+            <div className="h-dvh flex flex-col justify-center items-center  ">
+                <Typography variant="h3" color="">
+                    Loading ...
+                </Typography>
+                <Spinner className="h-24 w-24" color="indigo" />
+            </div>
+        );
+    }
+
+    if (isError) {
+        return (
+            <div className="h-dvh flex flex-col justify-center items-center  ">
+                <Typography variant="h3" color="red">
+                    {isError?.message}
+                </Typography>
+            </div>
+        );
+    }
+
     return (
         <>
-            <div className="flex flex-col justify-center items-center  bg-gray-600">
-                <Typography variant="h1" color="white">
+            <div className="flex flex-col justify-center items-center bg-blue-gray-900/50 ">
+                <Typography variant="h1" color="">
                     Git users
                 </Typography>
-                <Typography variant="paragraph" color="white">
+                <Typography variant="paragraph" color="">
                     API de users - react-vite - material taildwind
                 </Typography>
 
-                <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-4  m-2">
+                <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-6  m-2 ">
                     {user.map((item) => (
-                        <Card className=" w-80  sm:w-80 lg:w-96 " key={item.id}>
+                        <Card
+                            className=" w-80  sm:w-80 lg:w-72 shadow-md shadow-blue-gray-900/50"
+                            key={item.id}
+                        >
                             <img
                                 src={item.avatar_url}
                                 alt={item.login}
-                                className="mx-auto mt-5 h-48 w-48 rounded-full object-cover object-center shadow-xl shadow-blue-gray-900/50"
+                                className="mx-auto mt-5 h-48 w-48 rounded-full object-cover object-center shadow-md shadow-blue-gray-900/50"
                             />
                             <CardBody>
                                 <Typography
@@ -56,17 +92,19 @@ const App = () => {
                                 </Typography>
                             </CardBody>
 
-                            {/* <CardFooter className="pt-0">
-                                <Button
-                                    size="lg"
-                                    color="dark"
-                                    className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
-                                    ripple={false}
-                                    fullWidth={true}
-                                >
-                                    Profile Github
-                                </Button>
-                            </CardFooter> */}
+                            <CardFooter className="pt-0">
+                                <a href={item.html_url}>
+                                    <Button
+                                        color="amber"
+                                        size="lg"
+                                        className="hover:scale-[1.02] focus:scale-[1.02] active:scale-100"
+                                        ripple={true}
+                                        fullWidth={true}
+                                    >
+                                        Profile Github
+                                    </Button>
+                                </a>
+                            </CardFooter>
                         </Card>
                     ))}
                 </ul>
